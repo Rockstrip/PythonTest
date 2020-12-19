5c0812042d01349c7bf620e5fc83ae2b2e809b29
Rockstrip <artes5kgc@gmail.com>
2020-12-19 06:31:57 +0200

    first

---
+++ Python/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kapal.DESKTOP-MSIV0R8\source\repos\Python\Python\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
